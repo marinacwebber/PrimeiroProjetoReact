3db50f3a67f99ffd2c979909d55d9059808781e1
marinacwebber <105221777+marinacwebber@users.noreply.github.com>
2024-01-06 14:28:23 -0300

    adicionando pasta src

---
+++ src/telas/Cesta.js
@@ -0,0 +1,7 @@
+import { Text } from 'react-native'; //para poder usa o Text temos que importar
+
+export default function Cesta() {
+    return <Text>Cesta</Text>
+}
+
+// export pode utilizar varios componentes 
