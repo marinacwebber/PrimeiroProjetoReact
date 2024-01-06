82a8c0202b5ba53d6868468f0cec09362aefdb51
marinacwebber <105221777+marinacwebber@users.noreply.github.com>
2024-01-06 15:23:04 -0300

    Colocando imagem e texto de titulo

--- src/telas/Cesta.js
+++ src/telas/Cesta.js
@@ -1,7 +1,28 @@
-import { Text } from 'react-native'; //para poder usa o Text temos que importar
+import { Image, StyleSheet, Dimensions, Text } from 'react-native'; 
+import topo from '../../assets/topo.png';
+
+const width = Dimensions.get('screen').width;
 
 export default function Cesta() {
-    return <Text>Cesta</Text>
+  return <>
+    <Image source={topo} style={estilos.topo}/>
+    <Text style={estilos.titulo}>Detalhes da cesta</Text>
+  </>
 }
 
-// export pode utilizar varios componentes 
+const estilos = StyleSheet.create({
+  topo: {
+    width: "100%", //ocupa 100% da tela mas manteve a mesma altura
+    height: 578 / 768 * width,
+  },
+  titulo: {
+    width: "100%",
+    position: "absolute",
+    textAlign: "center",
+    color: "white",
+    fontSize: 16,
+    lineHeight: 26,
+    fontWeight: "bold",
+    padding: 16
+  }
+});
