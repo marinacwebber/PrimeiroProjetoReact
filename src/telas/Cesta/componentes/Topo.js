64c36e27666427c28f685ece71dccde09f056496
marinacwebber <105221777+marinacwebber@users.noreply.github.com>
2024-01-07 10:17:48 -0300

    Adicionando detalhes a componentes

--- src/telas/Cesta/componentes/Topo.js
+++ src/telas/Cesta/componentes/Topo.js
@@ -1,3 +1,30 @@
-export default function Too() {
-    return 
+import { Image, StyleSheet, Dimensions} from 'react-native';
+
+import Texto from '../../../componentes/Texto';
+import topo from '../../../../assets/topo.png';
+
+const width = Dimensions.get('screen').width;
+
+export default function Topo() {
+    return <>
+    <Image source={topo} style={estilos.topo}/>
+    <Texto style={estilos.titulo}>Detalhes da cesta</Texto>
+    </>
 }
+
+const estilos = StyleSheet.create({
+    topo: {
+        width: "100%", //ocupa 100% da tela mas manteve a mesma altura
+        height: 578 / 768 * width,
+      },
+      titulo: {
+        width: "100%",
+        position: "absolute",
+        textAlign: "center",
+        color: "white",
+        fontSize: 16,
+        lineHeight: 26,
+        fontWeight: "bold",
+        padding: 16
+      },
+})
--- src/telas/Cesta/index.js
+++ src/telas/Cesta/index.js
@@ -1,79 +1,20 @@
-import { Image, StyleSheet, Dimensions, View } from 'react-native'; 
+import { StyleSheet, View } from 'react-native'; 
 
-import Texto from '../../componentes/Texto';
-
-import topo from '../../../assets/topo.png';
-import logo from '../../../assets/logo.png';
-
-
-const width = Dimensions.get('screen').width;
+import Topo from './componentes/Topo';
+import Detalhes from './componentes/Detalhes';
 
 export default function Cesta() {
   return <>
-    <Image source={topo} style={estilos.topo}/>
-    <Texto style={estilos.titulo}>Detalhes da cesta</Texto>
-    
+    <Topo />
     <View style={estilos.cesta}>
-      <Texto style={estilos.nome}>Cesta de Verduras</Texto>
-        <View style={estilos.fazenda}>
-          <Image style={estilos.imagemFazenda} source={logo} />
-          <Texto style={estilos.nomeFazenda}>Jenny Jack Farm</Texto>
-        </View>        
-      <Texto style={estilos.descricao}>Uma cesta com produtos selecionados cuidadosamente da 
-            fazenda direto para sua cozinha</Texto>
-      <Texto style={estilos.preco}>R$ 40,00</Texto>
-    </View>  
+    <Detalhes /> 
+    </View>
   </>
 }
 
 const estilos = StyleSheet.create({
-  topo: {
-    width: "100%", //ocupa 100% da tela mas manteve a mesma altura
-    height: 578 / 768 * width,
-  },
-  titulo: {
-    width: "100%",
-    position: "absolute",
-    textAlign: "center",
-    color: "white",
-    fontSize: 16,
-    lineHeight: 26,
-    fontWeight: "bold",
-    padding: 16
-  },
   cesta: {
     paddingVertical: 8,
     paddingHorizontal: 26,
   },
-  nome: {
-    fontSize: 24,
-    lineHeight: 42,
-    color: "#464646",
-    fontWeight: "bold",
-  },
-  fazenda: {
-    flexDirection: "row",
-    paddingVertical: 12,    
-  },
-  imagemFazenda: {
-    width: 32,
-    height: 32,
-  },
-  nomeFazenda: {
-    fontSize: 16,
-    lineHeight: 26,
-    marginLeft: 12,
-  },
-  descricao: {
-    color: "#A3A3A3",
-    fontSize: 16,
-    lineHeight: 26
-  },
-  preco: {
-    color: "#2A9F85",
-    fontWeight: "bold",
-    fontSize: 26,
-    lineHeight: 42,
-    marginTop: 42
-  }
 });
