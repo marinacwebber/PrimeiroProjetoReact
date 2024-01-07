3eb48f687a6959864c010ddf756b6d6f803c439e
marinacwebber <105221777+marinacwebber@users.noreply.github.com>
2024-01-07 15:00:54 -0300

    textos generalizados

--- src/telas/Cesta/componentes/Detalhes.js
+++ src/telas/Cesta/componentes/Detalhes.js
@@ -1,17 +1,15 @@
 import { View, Image, StyleSheet } from 'react-native';
-import logo from '../../../../assets/logo.png';
 import Texto from '../../../componentes/Texto';
 
-export default function Detalhes() {
+export default function Detalhes({ nome, logoFazenda, nomeFazenda, descricao, preco}) {
     return <>
-    <Texto style={estilos.nome}>Cesta de Verduras</Texto>
+    <Texto style={estilos.nome}>{ nome }</Texto>
         <View style={estilos.fazenda}>
-          <Image style={estilos.imagemFazenda} source={logo} />
-          <Texto style={estilos.nomeFazenda}>Jenny Jack Farm</Texto>
+          <Image source={logoFazenda} style={estilos.imagemFazenda} />
+          <Texto style={estilos.nomeFazenda}>{ nomeFazenda }</Texto>
         </View>        
-      <Texto style={estilos.descricao}>Uma cesta com produtos selecionados cuidadosamente da 
-            fazenda direto para sua cozinha</Texto>
-      <Texto style={estilos.preco}>R$ 40,00</Texto>
+      <Texto style={estilos.descricao}>{ descricao }</Texto>
+      <Texto style={estilos.preco}>{ preco }</Texto>
     </>
 }
 
